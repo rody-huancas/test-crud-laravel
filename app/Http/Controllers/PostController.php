e92639460df93f29655bddc4df1478072e3d9d0c
Rody Huancas <rodyhuancas.04@gmail.com>
2025-07-21 22:11:51 -0500

    Feat: Implements the publication controller with methods for creating, storing and displaying resources

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\Post\CreatePostRequest;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CreatePostRequest $request)
+    {
+        Post::create([
+            "title"   => $request->input("title"),
+            "content" => $request->input("content"),
+            "status"  => $request->input("status")
+        ]);
+
+        return redirect()->route("posts.index")->with("message", "Post creado correctamente");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
