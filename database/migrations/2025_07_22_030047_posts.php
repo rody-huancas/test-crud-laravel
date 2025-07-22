4d5137b0875d3d5995f3a82cfe3bafc55564d4aa
Rody Huancas <rodyhuancas.04@gmail.com>
2025-07-21 22:11:18 -0500

    Featr: Adds migration for the ‘posts’ table with ‘title’, ‘content’ and ‘status’ fields. A new migration file is created for future modifications.

--- database/migrations/2025_07_22_030047_posts.php
+++ database/migrations/2025_07_22_030047_posts.php
@@ -11,13 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('content');
-            $table->enum('status', ["draft", "published"])->default("draft");
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -25,6 +19,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        //
     }
 };
